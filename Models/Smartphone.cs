36f711efb9f6cbcba69b98aa7d85b99b37b8fae4
Gustavotx2 <67023546+Gustavotx23@users.noreply.github.com>
2025-06-21 23:35:14 -0300

    Create Smartphone.cs

---
+++ Models/Smartphone.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; }
+
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public string Modelo { get; }
+        public string Imei { get; }
+        public int Memoria { get; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
